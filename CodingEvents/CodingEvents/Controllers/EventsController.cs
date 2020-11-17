ba055fe58eba6c845c41635ca6fe4cffec8825bb
jess-ward <jrnewport07@gmail.com>
2020-11-17 15:44:47 -0600

    added viewmodel

--- CodingEvents/CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/CodingEvents/Controllers/EventsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -17,24 +19,38 @@ namespace CodingEvents.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.events = Events;
-            return View();
+            List<Event> events = new List<Event> (EventData.GetAll());
+            return View(events);
         }
 
 
         [HttpGet]
         public IActionResult Add()
         {
-            return View();
+            AddEventViewModel addEventViewModel = new AddEventViewModel();
+            return View(addEventViewModel);
         }
 
 
         [HttpPost]
-        [Route("/Events/Add")]
-        public IActionResult NewEvent(string name)
+        public IActionResult Add(AddEventViewModel addEventViewModel)
         {
-            Events.Add(new Event(name));
+            Event newEvent = new Event
+            {
+                Name = addEventViewModel.Name,
+                Description = addEventViewModel.Description
+            };
+
+            EventData.Add(newEvent);
             return Redirect("/Events");
+        }
+
+
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+
+            return View();
         }
     }
 }
---
+++ CodingEvents/CodingEvents/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace CodingEvents.ViewModels
+{
+    public class AddEventViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
